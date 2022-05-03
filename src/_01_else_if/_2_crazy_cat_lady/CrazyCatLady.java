d54bd14e99cb837df8cea6b3d823169e2e1ea781
root <root@568015546d5d>
2022-05-03 01:25:07 +0000

    poopoopoopoopoopoopoopee

--- src/_01_else_if/_2_crazy_cat_lady/CrazyCatLady.java
+++ src/_01_else_if/_2_crazy_cat_lady/CrazyCatLady.java
@@ -3,18 +3,29 @@ package _01_else_if._2_crazy_cat_lady;
 
 import java.net.URI;
 
+import javax.swing.JOptionPane;
+
 public class CrazyCatLady {
 	public static void main(String[] args) {
 		
-		// 1. Ask the user how many cats they have
-		// 2. Convert their answer into an int
-		// 3. If they have 3 or more cats, tell them they are a crazy cat lady
+
+		String x = "httpswww.youtube.com/watch?v=QkZ3EPQHQsg";
+		String lol = JOptionPane.showInputDialog("how many dogs do you have");
+		int mami = Integer.parseInt(lol);
+		if(mami>2) {
+			JOptionPane.showInputDialog("you're a crazy dog person");
+		}
+		if(mami<3 && mami>0) {
+			
+			
+			//playVideo(x);
+		}
 		// 4. If they have less than 3 cats AND more than 0 cats, call the method below to show them a cat video
 		//    Hint: You can use the && operator 
 		// 5. If they have 0 cats, show them a video of A Frog Sitting on a Bench Like a Human
 		
 		
-		
+		//https://www.youtube.com/watch?v=QkZ3EPQHQsg
 		
 		
 		
--- src/_01_else_if/_3_high_low/HighLowGame.java
+++ src/_01_else_if/_3_high_low/HighLowGame.java
@@ -1,7 +1,8 @@
-
 package _01_else_if._3_high_low;
 
 import java.util.Random;
+
+import javax.swing.JOptionPane;
 
 public class HighLowGame {
 
@@ -10,11 +11,12 @@ public class HighLowGame {
 		int random = new Random().nextInt(5);
 		
 		// 2. Print out the random variable above
-		
+		System.out.println(random);
 		// 11. Repeat steps 1 to 10 ten times
 		
 			// 1. Ask the user for a guess using a pop-up window, and save their response 
-
+String number = JOptionPane.showInputDialog("take a guess on my number");
+int fart = Integer.parseInt(number);
 			// 4. Convert the users’ answer to an int (Integer.parseInt(string))
 			
 			// 5. if the guess is correct
