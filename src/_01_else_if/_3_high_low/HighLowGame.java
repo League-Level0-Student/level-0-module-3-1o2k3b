a23d3da4eb5efa5709b4910759f4d0d404c91a10
root <root@0ea377474812>
2022-05-10 01:22:46 +0000

    poopaloopa!!!

--- src/_01_else_if/_3_high_low/HighLowGame.java
+++ src/_01_else_if/_3_high_low/HighLowGame.java
@@ -1,3 +1,4 @@
+
 package _01_else_if._3_high_low;
 
 import java.util.Random;
@@ -11,14 +12,24 @@ public class HighLowGame {
 		int random = new Random().nextInt(5);
 		
 		// 2. Print out the random variable above
-		System.out.println(random);
+		
 		// 11. Repeat steps 1 to 10 ten times
 		
 			// 1. Ask the user for a guess using a pop-up window, and save their response 
-String number = JOptionPane.showInputDialog("take a guess on my number");
+		String number = JOptionPane.showInputDialog("take a guess on my number");
+		
 int fart = Integer.parseInt(number);
-			// 4. Convert the users’ answer to an int (Integer.parseInt(string))
-			
+
+		if(fart == random) {
+			JOptionPane.showInputDialog("you win.");	
+			}
+		if(fart > random) {
+			JOptionPane.showInputDialog("to high.");	
+			}
+		if(fart < random) {
+			JOptionPane.showInputDialog("to low.");	
+			}
+		
 			// 5. if the guess is correct
 				// 6. Win
 				// 12. Use "System.exit(0);" to quit the game if the user guessed the right answer.
---
+++ src/_01_else_if/_3_high_low/OlenClassOfHappinessLevels.java
@@ -0,0 +1,13 @@
+package _01_else_if._3_high_low;
+
+import javax.swing.JOptionPane;
+
+public class OlenClassOfHappinessLevels {
+public static void main(String[] args) {
+int x = JOptionPane.showMessageDialog(null, "are you happy?");
+int y = Integer.parseInt(x);
+	if(y = yes);	
+}
+}
+
+
