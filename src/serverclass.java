6d88f5d5b14ef87d76e69f8ff349112e5fa8a8a6
root <root@e81a5865fc5b>
2022-06-07 00:48:56 +0000

    yesnoyesnoyayaahh

--- src/serverclass.java
+++ src/serverclass.java
@@ -1,31 +1,39 @@
 import javax.swing.JOptionPane;
 
 public class serverclass {
-public static void main(String[] args) {
-	String x = JOptionPane.showInputDialog("to start your adventure, your friend asked you to go explore the ancient caverns.");
+	public static void main(String[] args) {
+		String x = JOptionPane
+				.showInputDialog("to start your adventure, your friend asked you to go explore the ancient caverns.");
 
-	if(x.equals ("yes")) {
-JOptionPane.showInputDialog("you and your friend go into the cave, and after a half hour of walking, both of you are terribly lost");
-String w = JOptionPane.showInputDialog("Do you bring out your sword or not?");
-if(w.equals("yes")) {
-JOptionPane.showInputDialog("when you bring out your sword, you notice it's ruby kind of looks like a button.");	
-}
-else if(w.equals("no")) {
-String a = JOptionPane.showInputDialog("you and your friend wonder around till you find a ruby in the wall, do you take it out?");
-if(a.equals("yes")) {
-String c = JOptionPane.showInputDialog("when you take it out, you realize it looks just like the ruby on your sword, but you have no time to marvel at it, because the ruby reveals a giant chamber with a giant spider inside, do you run, or break through the chamber and fight the spider?");
-	
-}
-}
+		if (x.equals("yes")) {
+			JOptionPane.showInputDialog(
+					"you and your friend go into the cave, and after a half hour of walking, both of you are terribly lost");
+			String w = JOptionPane.showInputDialog("Do you bring out your sword or not?");
+			if (w.equals("yes")) {
+				JOptionPane.showInputDialog(
+						"when you bring out your sword, you notice it's ruby kind of looks like a button.");
+			} else if (w.equals("no")) {
+				String a = JOptionPane.showInputDialog(
+						"you and your friend wonder around till you find a ruby in the wall, do you take it out?");
+				if (a.equals("yes")) {
+					String c = JOptionPane.showInputDialog(
+							"when you take it out, you realize it looks just like the ruby on your sword, but you have no time to marvel at it, because the ruby reveals a giant chamber with a giant spider inside, do you run,(yes) or break through the chamber and fight the spider?(no)");
+					if (c.equals("yes")) {
+						String j = JOptionPane.showInputDialog(
+								"as you run, the spider hears your feet, and gives a chase, do you deeper into the caves,(no) or fight(yes)");
+						
+					}
+					else if (c.equals("no")) {
+						String v = JOptionPane.showInputDialog(
+								"you and your friend both give a fight, but you seem to be losing, until you give up to the spider, and as you are about to get killed, your friend trips over a rock and lands on your sword, do you say push the ruby or not?");
+					}
 
+				}
+			}
 
+		} else if (x.equals("no")) {
+			JOptionPane.showInputDialog("your friend still explores, and finds 1 trillion dollars worth of daimonds");
+		}
 
 	}
-else if(x.equals("no")) {
-	JOptionPane.showInputDialog("your friend still explores, and finds 1 trillion dollars worth of daimonds");
-}
-	
-	
-
-}
 }
