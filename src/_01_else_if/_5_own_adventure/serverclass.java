ed47bed02a89d95d35d9be4947b082346bde383e
root <root@4f5e3c689bba>
2022-07-12 01:18:33 +0000

    rushhourgottagobye

--- src/_01_else_if/_5_own_adventure/serverclass.java
+++ src/_01_else_if/_5_own_adventure/serverclass.java
@@ -1,3 +1,4 @@
+package _01_else_if._5_own_adventure;
 import javax.swing.JOptionPane;
 
 public class serverclass {
---
+++ src/_02_loop_variables/_1_years_alive/serverclass_2.java
@@ -0,0 +1,9 @@
+package _02_loop_variables._1_years_alive;
+
+public class serverclass_2 {
+public static void main(String[] args) {
+	for(int i=1; i<2023; i++) {
+		System.out.println(i);
+	}
+}
+}
